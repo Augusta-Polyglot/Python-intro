58057c730fcb730eb7f0f556ba88e6918e78794e
James Sturtevant <jsturtevant@gmail.com>
2014-11-16 09:54:06 -0500

    added title

--- UI/tkinterExample.py
+++ UI/tkinterExample.py
@@ -22,6 +22,7 @@ class Application(Frame):
 
     def __init__(self, master=None):
         Frame.__init__(self, master)
+        self.master.title("Log Parser")
         self.pack()
         self.createWidgets()
 
