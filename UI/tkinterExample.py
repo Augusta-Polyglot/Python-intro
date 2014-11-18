fe099a2c6f83f454100c522aee1ef279835b02f6
James Sturtevant <jsturtevant@gmail.com>
2014-11-17 19:21:52 -0500

    added a way to show selected line info

--- UI/tkinterExample.py
+++ UI/tkinterExample.py
@@ -39,6 +39,15 @@ class Application(tk.Frame):
         self.FileListBox = tk.Listbox(self)
         self.FileListBox.grid(row = 2, column= 0, columnspan = 2 , sticky= tk.E + tk.W)
         self.FileListBox.insert(tk.END, "a list entry")
+        self.FileListBox.insert(tk.END, "another list entry")
+
+        self.CurrentLineInfoLabel = tk.Label(self)
+        self.CurrentLineInfoLabel["text"] = "Line Info:"
+        self.CurrentLineInfoLabel.grid(row = 3, column=0, sticky = tk.W)
+
+        self.CurrentLineInfo = tk.Label(self)
+        self.CurrentLineInfo.grid(row = 3, column=1)
+        self.CurrentLineInfo["text"] = "Select a line above"
 
         self.SetupQuit()
     def __init__(self, master=None):
@@ -50,6 +59,21 @@ class Application(tk.Frame):
         #makes grid expand
         self.grid(sticky=tk.N+tk.S+tk.E+tk.W)
         self.createWidgets()
+
+        self.current = None
+        self.poll()
+
+    def poll(self):
+        now = self.FileListBox.curselection()
+        if now != self.current:
+            self.list_has_changed(now)
+            self.current = now
+        self.after(250, self.poll)
+
+    def list_has_changed(self, selection):
+        print "selection is", selection
+        if len(selection) > 0:
+            self.CurrentLineInfo["text"] = self.FileListBox.get(selection[0])
 
 root = tk.Tk()
 app = Application(master=root)
